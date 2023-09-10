fcb75ea0d6885471c7a7a6ac0e18ef3ea73f8b4e
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-09-10 20:52:59 +0700

    no message

--- src/config/connection.database.js
+++ src/config/connection.database.js
@@ -3,11 +3,12 @@ import mysql from "mysql";
 const getConnection = () => {
     return mysql.createConnection({
         // host: "127.0.0.1",
-        host: "192.168.1.18",
+        host: "192.168.1.16",
         port: 3306,
         user: "root",
         password: "password",
         database: "module3-project",
+        // connectTimeout: 10000, // in milliseconds, so 10 seconds
     });
 };
 
